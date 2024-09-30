bbb0dbe1b0b5aef1a80b5dc6b4c9b461a88912e2
ImmortalIdiot <max.krasulya@yandex.ru>
2024-09-30 20:36:08 +0300

    refactor: inject the necessary implementation depending on the time

--- src/main/java/com/immortalidiot/SpringDI/config/GreetingConfig.java
+++ src/main/java/com/immortalidiot/SpringDI/config/GreetingConfig.java
@@ -1,10 +1,14 @@
 package com.immortalidiot.SpringDI.config;
 
 import com.immortalidiot.SpringDI.service.GreetingService;
+import com.immortalidiot.SpringDI.service.impl.AfternoonGreetingService;
+import com.immortalidiot.SpringDI.service.impl.EveningGreetingService;
 import com.immortalidiot.SpringDI.service.impl.MorningGreetingService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+
+import java.time.LocalTime;
 
 @Configuration
 public class GreetingConfig {
@@ -12,6 +16,16 @@ public class GreetingConfig {
     @Bean
     @Scope("prototype")
     public GreetingService greetingService() {
-        return new MorningGreetingService();
+        LocalTime nowTime = LocalTime.now();
+
+        if (nowTime.isAfter(LocalTime.of(6, 0)) &&
+                nowTime.isBefore(LocalTime.of(12, 0))) {
+            return new MorningGreetingService();
+        } else if (nowTime.isAfter(LocalTime.of(12, 0)) &&
+                nowTime.isBefore(LocalTime.of(18, 0))) {
+            return new AfternoonGreetingService();
+        } else {
+            return new EveningGreetingService();
+        }
     }
 }
